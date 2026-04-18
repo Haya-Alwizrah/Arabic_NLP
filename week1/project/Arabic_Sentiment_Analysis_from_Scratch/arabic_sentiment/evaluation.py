bb09f35aaec05050c864de68012a9e77d91320c8
HayaAlwizrah <hayaalwizrah@gmail.com>
2026-04-19 01:06:06 +0300

    Update evaluation.py

--- week1/project/Arabic_Sentiment_Analysis_from_Scratch/arabic_sentiment/evaluation.py
+++ week1/project/Arabic_Sentiment_Analysis_from_Scratch/arabic_sentiment/evaluation.py
@@ -2,8 +2,15 @@ from typing import List, Tuple
 
 def accuracy(predictions: List[str], gold: List[str]) -> float:
     """Compute accuracy as the fraction of correct predictions."""
-    # TODO: implement
-    raise NotImplementedError
+    c = 0
+    total = len(gold)
+
+    for p,g in zip(predictions, gold):
+        if p ==g:
+            c += 1
+    score = c / total
+    return score
+
 
 def precision_recall_f1(predictions: List[str], gold: List[str], positive_label: str = "positive") -> Tuple[float, float, float]:
     """
@@ -12,8 +19,29 @@ def precision_recall_f1(predictions: List[str], gold: List[str], positive_label:
     Returns:
         (precision, recall, f1) as a tuple of floats.
     """
-    # TODO: implement without sklearn
-    raise NotImplementedError
+    # precision = tp / (tp+fp) --> retrived relevent/ all retrived
+    # recall = tp / (tp+fn) -->  retrived relevent/ all relevent
+    # f1 = 2* (precision*recall)/(precision+recall) 
+    tp = 0 # retrived relevent
+    fp = 0 # retrived irelevent
+    fn = 0 # not retrived relevent
+
+    for p, g in zip(predictions, gold):
+        if p == positive_label and g == positive_label:
+            tp += 1
+        elif p == positive_label and g != positive_label:
+            fp += 1
+        elif p != positive_label and g == positive_label:
+            fn += 1
+
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    f1 = 2 * (precision * recall) / (precision + recall)
+        
+    return precision, recall, f1
+
+
+
 
 def confusion_matrix_str(predictions: List[str], gold: List[str], labels: List[str]) -> str:
     """
@@ -24,5 +52,19 @@ def confusion_matrix_str(predictions: List[str], gold: List[str], labels: List[s
     True Pos     42        8
     True Neg      5       45
     """
-    # TODO: implement
-    raise NotImplementedError
+    m = {}
+    for actual in labels:
+        m[actual] = {}
+        for pred in labels:
+            m[actual][pred] = 0
+
+    for p, g in zip(predictions, gold):
+        if p in labels and g in labels:
+            m[g][p] += 1
+
+    header = f"{' ':12} | {'Pred ' + labels[0]:10} | {'Pred ' + labels[1]:10}"
+    line = "-" * len(header)
+    row1 = f"{'True ' + labels[0]:12} | {m[labels[0]][labels[0]]:10} | {m[labels[0]][labels[1]]:10}"
+    row2 = f"{'True ' + labels[1]:12} | {m[labels[1]][labels[0]]:10} | {m[labels[1]][labels[1]]:10}"
+    
+    return f"{header}\n{line}\n{row1}\n{row2}"
