8ce1d0648efecce6e6423db5aec092002590b4fa
HayaAlwizrah <hayaalwizrah@gmail.com>
2026-04-19 01:28:37 +0300

    Update main.py

--- week1/project/Arabic_Sentiment_Analysis_from_Scratch/main.py
+++ week1/project/Arabic_Sentiment_Analysis_from_Scratch/main.py
@@ -1,54 +1,57 @@
+import random
 from datasets import load_dataset
 
-ds = load_dataset("arbml/Arabic_Sentiment_Twitter_Corpus")
+from arabic_sentiment.preprocessing import ArabicPreprocessor
+from arabic_sentiment.language_model import NgramLanguageModel
+from arabic_sentiment.naive_bayes import NaiveBayesClassifier
+from arabic_sentiment.evaluation import accuracy, precision_recall_f1, confusion_matrix_str
 
+
+ds = load_dataset("arbml/Arabic_Sentiment_Twitter_Corpus")
 train_data = ds["train"]
 test_data  = ds["test"]
 
-import re
-from arabic_sentiment.preprocessing import ArabicPreprocessor
-from arabic_sentiment.language_model import NgramLanguageModel
+# ----------------------------------- Pre Processing -----------------------------------------
+pre_processor = ArabicPreprocessor()
 
-
-p = ArabicPreprocessor()
-
-train_data = train_data.map(lambda x: {"clean_tweet": p.preprocess(x["tweet"])})
-
-model_1 = NgramLanguageModel(2)
-model_2 = NgramLanguageModel(2)
-model_3 = NgramLanguageModel(3)
-model_4 = NgramLanguageModel(3)
+train_data = train_data.map(lambda x: {"clean_tweet": pre_processor.preprocess(x["tweet"])})
+test_data = test_data.map(lambda x: {"clean_tweet": pre_processor.preprocess(x["tweet"])})
 
 raw_train = [x.split() for x in train_data["tweet"]]
 clean_train = train_data["clean_tweet"]
-
-model_1.train(raw_train)
-model_2.train(clean_train)
-model_3.train(raw_train)
-model_4.train(clean_train)
-
-
-test_data = test_data.map(lambda x: {"clean_tweet": p.preprocess(x["tweet"])})
 raw_test = [x.split() for x in test_data["tweet"]]
 clean_test = test_data["clean_tweet"]
 
-p_raw1 = model_1.perplexity(raw_test)
-p_clean1 = model_2.perplexity(clean_test)
-p_raw2 = model_3.perplexity(raw_test)
-p_clean2 = model_4.perplexity(clean_test)
+# ------------------------------------- Language Model -------------------------------------------
+bigram_raw = NgramLanguageModel(2)
+bigram_pre = NgramLanguageModel(2)
+trigram_raw = NgramLanguageModel(3)
+trigram_pre = NgramLanguageModel(3)
+
+# train with train data
+bigram_raw.train(raw_train)
+bigram_pre.train(clean_train)
+trigram_raw.train(raw_train)
+trigram_pre.train(clean_train)
+
+# perplexity with test data
+bigram_p_raw = bigram_raw.perplexity(raw_test)
+bigram_p_pre = bigram_pre.perplexity(clean_test)
+trigram_p_raw = trigram_raw.perplexity(raw_test)
+trigram_p_pre = trigram_pre.perplexity(clean_test)
 
 print("\n" + "="*45)
 print(f"{'Model':<15} | {'Raw PPL':<12} | {'Preprocessed':<12}")
 print("-" * 45)
-print(f"{'Bigram':<15} | {p_raw1:<12.2f} | {p_clean1:<12.2f}")
-print(f"{'Trigram':<15} | {p_raw2:<12.2f} | {p_clean2:<12.2f}")
+print(f"{'Bigram':<15} | {bigram_p_raw:<12.2f} | {bigram_p_pre:<12.2f}")
+print(f"{'Trigram':<15} | {trigram_p_raw:<12.2f} | {trigram_p_pre:<12.2f}")
 print("="*45 + "\n")
 
 models = {
-    "Bigram (Raw)": model_1,
-    "Bigram (Clean)": model_2,
-    "Trigram (Raw)": model_3,
-    "Trigram (Clean)": model_4
+    "Bigram (raw)": bigram_raw,
+    "Bigram (Clean)": bigram_pre,
+    "Trigram (Raw)": trigram_raw,
+    "Trigram (Clean)": trigram_pre
 }
 
 for name, model in models.items():
@@ -58,10 +61,7 @@ for name, model in models.items():
         print(f"{i+1}. {sample}")
     print("-" * 30)
 
-from arabic_sentiment.naive_bayes import NaiveBayesClassifier
-from arabic_sentiment.evaluation import accuracy, precision_recall_f1, confusion_matrix_str
-import random
-
+# ------------------------------------------- Naive Bayes Classifier -----------------------------------------------
 naiv = NaiveBayesClassifier()
 naiv.train(clean_train, train_data["label"])
 
