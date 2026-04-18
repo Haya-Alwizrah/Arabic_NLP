d6152b0d27d80e69df07825340eaa5e71e3325c7
HayaAlwizrah <hayaalwizrah@gmail.com>
2026-04-19 01:17:02 +0300

    Update main.py

--- week1/project/Arabic_Sentiment_Analysis_from_Scratch/main.py
+++ week1/project/Arabic_Sentiment_Analysis_from_Scratch/main.py
@@ -57,3 +57,55 @@ for name, model in models.items():
         sample = model.generate()
         print(f"{i+1}. {sample}")
     print("-" * 30)
+
+from arabic_sentiment.naive_bayes import NaiveBayesClassifier
+from arabic_sentiment.evaluation import accuracy, precision_recall_f1, confusion_matrix_str
+import random
+
+naiv = NaiveBayesClassifier()
+naiv.train(clean_train, train_data["label"])
+
+random.seed(42)
+test_indices = random.sample(range(len(test_data)), 100)
+sample_test = test_data.select(test_indices)
+
+sample_texts = [x.split() for x in sample_test["clean_tweet"]]
+gold_labels = sample_test["label"]
+original_tweets = sample_test["tweet"]
+
+predictions = naiv.predict(sample_texts)
+
+acc = accuracy(predictions, gold_labels)
+prec, rec, f1 = precision_recall_f1(predictions, gold_labels, positive_label="pos")
+
+print("--- CLASSIFICATION REPORT ---")
+print(f"Accuracy  : {acc}")
+print(f"Precision : {prec}")
+print(f"Recall    : {rec}")
+print(f"F1-Score  : {f1}")
+
+print("\n--- CONFUSION MATRIX ---")
+cm_str = confusion_matrix_str(predictions, gold_labels, labels=["pos", "neg"])
+print(cm_str)
+
+correct_examples = []
+incorrect_examples = []
+
+for i in range(len(predictions)):
+    item = {
+        "tweet": original_tweets[i],
+        "true": gold_labels[i],
+        "pred": predictions[i]
+    }
+    if predictions[i] == gold_labels[i]:
+        correct_examples.append(item)
+    else:
+        incorrect_examples.append(item)
+
+print("\n" + "="*30 + " 5 CORRECT PREDICTIONS " + "="*30)
+for i, ex in enumerate(correct_examples[:5]):
+    print(f"{i+1}. [True: {ex['true']} | Pred: {ex['pred']}] -> {ex['tweet']}")
+
+print("\n" + "="*30 + " 5 INCORRECT PREDICTIONS " + "="*30)
+for i, ex in enumerate(incorrect_examples[:5]):
+    print(f"{i+1}. [True: {ex['true']} | Pred: {ex['pred']}] -> {ex['tweet']}")
