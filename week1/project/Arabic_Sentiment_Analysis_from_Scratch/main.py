b8a03d2642ad71a6b46130b112e11c2c74ab9b0b
HayaAlwizrah <hayaalwizrah@gmail.com>
2026-04-18 17:19:37 +0300

    main preprocessing

--- week1/project/Arabic_Sentiment_Analysis_from_Scratch/main.py
+++ week1/project/Arabic_Sentiment_Analysis_from_Scratch/main.py
@@ -4,3 +4,16 @@ ds = load_dataset("arbml/Arabic_Sentiment_Twitter_Corpus")
 
 train_data = ds["train"]
 test_data  = ds["test"]
+
+import re
+from arabic_sentiment.preprocessing import ArabicPreprocessor
+
+p = ArabicPreprocessor()
+
+ds2 = []
+for d in train_data["tweet"]:
+    ds2.append(p.preprocess(d))
+
+for i in zip(train_data["tweet"][:5],ds2[:5]):
+    print(i)
+
