97c1d0c174ab9e26f13b03be95f5ef66b56c7261
HayaAlwizrah <hayaalwizrah@gmail.com>
2026-04-24 16:06:53 +0300

    ش

--- week2/Project/main.py
+++ week2/Project/main.py
@@ -10,4 +10,7 @@ ds = load_dataset("arbml/Arabic_Sentiment_Twitter_Corpus")
 
 pre_processor = ArabicPreprocessor()
 data = ds.map(lambda x: {"clean_tweet": pre_processor.preprocess(x["tweet"])})
+train_data = data["train"].remove_columns(["tweet"])
+test_data  = data["test"].remove_columns(["tweet"])
+
 
