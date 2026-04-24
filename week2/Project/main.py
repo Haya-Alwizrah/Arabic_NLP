68516fb835b8ab44aa5284a8a486b7ed4a51cf35
HayaAlwizrah <hayaalwizrah@gmail.com>
2026-04-24 16:09:09 +0300

    main

--- week2/Project/main.py
+++ week2/Project/main.py
@@ -1,6 +1,6 @@
 from datasets import load_dataset
 from preprocessing import ArabicPreprocessor
-
+from Arab_w2v import Arab_w2v
 
 
 ds = load_dataset("arbml/Arabic_Sentiment_Twitter_Corpus")
@@ -13,4 +13,11 @@ data = ds.map(lambda x: {"clean_tweet": pre_processor.preprocess(x["tweet"])})
 train_data = data["train"].remove_columns(["tweet"])
 test_data  = data["test"].remove_columns(["tweet"])
 
+# --------------------------------------- w2v ---------------------------------
+
+w2v = Arab_w2v(100,5,1,0)
+w2v.train(train_data["clean_tweet"])
+print(w2v.most_similar("رمضان"))
+print(w2v.similarity("الهلال", "النصر"))
+w2v.save("w2v.model")
 
